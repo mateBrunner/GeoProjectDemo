115f5c1e9ec15966aca2dd61a7958f37b2fca39f
Brunner Máté <mbrunner@geometria.hu>
2021-01-07 16:51:39 +0100

    proxy config

---
+++ GeoProjectDemo/Connected Services/GeoProjectServiceTeszt/ReferenceExtension.cs
@@ -0,0 +1,57 @@
+﻿using BaseClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.ServiceModel.Channels;
+using System.Threading.Tasks;
+
+namespace GeoProjectServiceTeszt
+{
+    public partial class GeoProjectServiceClient : System.ServiceModel.ClientBase<GeoProjectServiceTeszt.GeoProjectService>, GeoProjectServiceTeszt.GeoProjectService
+    {
+
+        public GeoProjectServiceClient( ServiceOptions options ) :
+                base( GetCustomBinding( ), GetDefaultEndpointAddress( ) )
+        {
+
+            this.Endpoint.Name = EndpointConfiguration.BasicHttpBinding_GeoProjectService.ToString( );
+
+            var webProxy = new WebProxy( $"{options.ProxyAddress}:{options.ProxyPort}", true );
+            webProxy.Credentials = new NetworkCredential( options.ProxyUsername, options.ProxyPassword );
+            WebRequest.DefaultWebProxy = webProxy;
+
+            ClientCredentials.UserName.UserName = options.Username;
+            ClientCredentials.UserName.Password = options.Password;
+
+            ConfigureEndpoint( this.Endpoint, this.ClientCredentials );
+
+        }
+
+        private static System.ServiceModel.Channels.Binding GetCustomBinding( )
+        {
+            var endpointConfiguration = EndpointConfiguration.BasicHttpBinding_GeoProjectService;
+
+            if ( ( endpointConfiguration == EndpointConfiguration.BasicHttpBinding_GeoProjectService ) )
+            {
+                System.ServiceModel.BasicHttpBinding binding = new System.ServiceModel.BasicHttpBinding( );
+                binding.MaxBufferSize = int.MaxValue;
+                binding.ReaderQuotas = System.Xml.XmlDictionaryReaderQuotas.Max;
+                binding.MaxReceivedMessageSize = int.MaxValue;
+                binding.AllowCookies = true;
+                binding.Security.Mode = System.ServiceModel.BasicHttpSecurityMode.TransportCredentialOnly;
+                binding.Security.Transport.ClientCredentialType = System.ServiceModel.HttpClientCredentialType.Windows;
+
+                var customBinding = new CustomBinding( binding );
+                var htbe = customBinding.Elements.Find<HttpTransportBindingElement>( );
+
+                htbe.ProxyAuthenticationScheme = AuthenticationSchemes.Basic;
+
+                htbe.UseDefaultWebProxy = false;
+
+                return customBinding;
+            }
+            throw new System.InvalidOperationException( string.Format( "Could not find endpoint with name \'{0}\'.", endpointConfiguration ) );
+        }
+    }
+}
--- GeoProjectDemo/Globals/Globals.cs
+++ GeoProjectDemo/Globals/Globals.cs
@@ -28,100 +28,103 @@ namespace GeoProjectDemo.Globals
                 if ( m_ProjectServiceTeszt != null )
                     return m_ProjectServiceTeszt;
 
-                m_ProjectServiceTeszt = new GeoProjectServiceTeszt.GeoProjectServiceClient( );
 
                 var options = m_ServiceOptions.FirstOrDefault( s => s.ServiceName == "GeoProjectService" );
 
-                if ( options != null )
-                {
-                    m_ProjectServiceTeszt.ClientCredentials.UserName.UserName = options.Username;
-                    m_ProjectServiceTeszt.ClientCredentials.UserName.Password = options.Password;
-                } else
-                //az első hívásnál eldöntjük, hogy azért nincs kitöltve, mert nincs authentikáció
-                {
-                    m_ProjectServiceTeszt.ClientCredentials.UserName.UserName = Environment.UserName;
-                }
+                m_ProjectServiceTeszt = new GeoProjectServiceTeszt.GeoProjectServiceClient( options );
+
+
+
+                //if ( options != null )
+                //{
+                //    m_ProjectServiceTeszt.ClientCredentials.UserName.UserName = options.Username;
+                //    m_ProjectServiceTeszt.ClientCredentials.UserName.Password = options.Password;
+                //} else
+                ////az első hívásnál eldöntjük, hogy azért nincs kitöltve, mert nincs authentikáció
+                //{
+                //    m_ProjectServiceTeszt.ClientCredentials.UserName.UserName = Environment.UserName;
+                //}
 
 
 #region Version1
-                //var b_custom = m_ProjectServiceTeszt.Endpoint.Binding as System.ServiceModel.Channels.CustomBinding;
+                ////var b_custom = m_ProjectServiceTeszt.Endpoint.Binding as System.ServiceModel.Channels.CustomBinding;
 
-                //if ( b_custom != null )
-                //{
-                //    var htbe = b_custom.Elements.Find<HttpTransportBindingElement>( );
-                //    htbe.ProxyAddress = new Uri( string.Format( "http://{0}:{1}", "localhost", "8080" ) );
-                //    htbe.ProxyAuthenticationScheme = System.Net.AuthenticationSchemes.Anonymous; // Or whatever authentication mechanism your proxy server uses
-                //    htbe.UseDefaultWebProxy = false;
+                ////if ( b_custom != null )
+                ////{
+                ////    var htbe = b_custom.Elements.Find<HttpTransportBindingElement>( );
+                ////    htbe.ProxyAddress = new Uri( string.Format( "http://{0}:{1}", "localhost", "8080" ) );
+                ////    htbe.ProxyAuthenticationScheme = System.Net.AuthenticationSchemes.Anonymous; // Or whatever authentication mechanism your proxy server uses
+                ////    htbe.UseDefaultWebProxy = false;
 
-                //}
+                ////}
 
-                var binding = new BasicHttpBinding( );
+                //var binding = new BasicHttpBinding( );
 
-                //binding.Name = "DEEWR_Customer_OUTBinding";
-                //binding.AllowCookies = false;
-                //binding.SendTimeout = new TimeSpan(0, 10, 0);
-                //binding.ReceiveTimeout = new TimeSpan( 0, 10, 0 );
-                //binding.OpenTimeout = new TimeSpan( 0, 10, 0 );
-                //binding.CloseTimeout = new TimeSpan( 0, 10, 0 );
-                //binding.MaxBufferPoolSize = 2147483647;
-                //binding.MaxReceivedMessageSize = 2147483647;
-                //binding.TextEncoding = Encoding.UTF8;
-                //binding.TransferMode = TransferMode.Buffered;
-                //binding.BypassProxyOnLocal = false;
-                //binding.UseDefaultWebProxy = false;
+                ////binding.Name = "DEEWR_Customer_OUTBinding";
+                ////binding.AllowCookies = false;
+                ////binding.SendTimeout = new TimeSpan( 0, 10, 0 );
+                ////binding.ReceiveTimeout = new TimeSpan( 0, 10, 0 );
+                ////binding.OpenTimeout = new TimeSpan( 0, 10, 0 );
+                ////binding.CloseTimeout = new TimeSpan( 0, 10, 0 );
+                ////binding.MaxBufferPoolSize = 2147483647;
+                ////binding.MaxReceivedMessageSize = 2147483647;
+                ////binding.TextEncoding = Encoding.UTF8;
+                ////binding.TransferMode = TransferMode.Buffered;
+                ////binding.BypassProxyOnLocal = false;
+                ////binding.UseDefaultWebProxy = false;
 
-                //binding.ReaderQuotas.MaxDepth = 32;
-                //binding.ReaderQuotas.MaxStringContentLength = 5242880;
-                //binding.ReaderQuotas.MaxArrayLength = 16384;
-                //binding.ReaderQuotas.MaxBytesPerRead = 4096;
-                //binding.ReaderQuotas.MaxNameTableCharCount = 16384;
+                ////binding.ReaderQuotas.MaxDepth = 32;
+                ////binding.ReaderQuotas.MaxStringContentLength = 5242880;
+                ////binding.ReaderQuotas.MaxArrayLength = 16384;
+                ////binding.ReaderQuotas.MaxBytesPerRead = 4096;
+                ////binding.ReaderQuotas.MaxNameTableCharCount = 16384;
 
-                //binding.Security.Mode = BasicHttpSecurityMode.Transport;
-                //binding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Certificate;
+                ////binding.Security.Mode = BasicHttpSecurityMode.Transport;
+                ////binding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Certificate;
 
-                var customBinding = new CustomBinding( binding );
-                var htbe = customBinding.Elements.Find<HttpTransportBindingElement>( );
+                //var customBinding = new CustomBinding( binding );
+                //var htbe = customBinding.Elements.Find<HttpTransportBindingElement>( );
 
-                var webProxy = new WebProxy( $"{options.ProxyAddress}/{options.ProxyPort}", true );
-                webProxy.Credentials = new NetworkCredential( options.ProxyUsername, options.ProxyPassword );
-                WebRequest.DefaultWebProxy = webProxy;
+                ////var webProxy = new WebProxy( $"{options.ProxyAddress}/{options.ProxyPort}", true );
+                ////webProxy.Credentials = new NetworkCredential( options.ProxyUsername, options.ProxyPassword );
+                ////WebRequest.DefaultWebProxy = webProxy;
 
 
-                htbe.ProxyAddress = new Uri( "http://localhost:8080" );
-                htbe.ProxyAuthenticationScheme = AuthenticationSchemes.Basic;
+                ////htbe.ProxyAddress = new Uri( "http://localhost:8080" );
+                ////htbe.ProxyAuthenticationScheme = AuthenticationSchemes.Basic;
 
-                htbe.UseDefaultWebProxy = false;
+                ////htbe.UseDefaultWebProxy = false;
 
-                m_ProjectServiceTeszt.Endpoint.Binding = customBinding;
+                //m_ProjectServiceTeszt.Endpoint.Binding = customBinding;
 #endregion
 
 #region Version2
 
-                var binding2 = new CustomBinding( );
+                //var binding2 = new CustomBinding( );
 
-                TransportSecurityBindingElement securityBindingElement = SecurityBindingElement.CreateUserNameOverTransportBindingElement( );
+                //TransportSecurityBindingElement securityBindingElement = SecurityBindingElement.CreateUserNameOverTransportBindingElement( );
 
-                var secureTransport = new HttpsTransportBindingElement( );
-                secureTransport.UseDefaultWebProxy = false;
-                secureTransport.ProxyAddress = new Uri( $"{options.ProxyAddress}/{options.ProxyPort}" );
-                secureTransport.ProxyAuthenticationScheme = AuthenticationSchemes.Basic;
+                //var secureTransport = new HttpsTransportBindingElement( );
+                //secureTransport.UseDefaultWebProxy = false;
+                //secureTransport.ProxyAddress = new Uri( $"{options.ProxyAddress}/{options.ProxyPort}" );
+                //secureTransport.ProxyAuthenticationScheme = AuthenticationSchemes.Basic;
 
-                binding2.Elements.Add( securityBindingElement );
-                binding2.Elements.Add( secureTransport );
+                //binding2.Elements.Add( securityBindingElement );
+                //binding2.Elements.Add( secureTransport );
 
-                var endpointAddress = new EndpointAddress( options.URL );
+                //var endpointAddress = new EndpointAddress( options.URL );
 
-                var factory = new ChannelFactory<GeoProjectServiceTeszt>( binding2, endpointAddress );
+                //var factory = new ChannelFactory<GeoProjectServiceTeszt>( binding2, endpointAddress );
 
-                // Credentials for authentication against the remote service
-                factory.Credentials.UserName.UserName = options.Username;
-                factory.Credentials.UserName.Password = options.Password;
+                //// Credentials for authentication against the remote service
+                //factory.Credentials.UserName.UserName = options.Username;
+                //factory.Credentials.UserName.Password = options.Password;
 
-                // Credentials for authentication against the proxy server
-                factory.Credentials.Windows.ClientCredential.UserName = options.ProxyUsername;
-                factory.Credentials.Windows.ClientCredential.Password = options.ProxyPassword;
+                //// Credentials for authentication against the proxy server
+                //factory.Credentials.Windows.ClientCredential.UserName = options.ProxyUsername;
+                //factory.Credentials.Windows.ClientCredential.Password = options.ProxyPassword;
 
-                var client = factory.CreateChannel( );
+                //var client = factory.CreateChannel( );
 
 #endregion
 
